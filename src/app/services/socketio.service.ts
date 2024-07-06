1042770ea42b02113dffc62f1e5832a770c9f05f
Sharath <sharathj116@gmail.com>
2024-07-06 12:19:22 +0530

    url updated in socket service

--- src/app/services/socketio.service.ts
+++ src/app/services/socketio.service.ts
@@ -11,7 +11,7 @@ export class SocketService {
   private socket: Socket;
 
   constructor() {
-    this.socket = io("http://localhost:5000"); 
+    this.socket = io('https://furnishnest.online'); 
     this.socket.on('connect', () => {
       console.log('Socket connected:', this.socket.id);
     });
