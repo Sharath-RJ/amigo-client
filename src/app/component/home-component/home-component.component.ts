b7d3759d43cfd357e119f65b07bd6f0a45e9d6fa
Sharath <sharathj116@gmail.com>
2024-07-06 15:12:37 +0530

    front end updated for not count

--- src/app/component/home-component/home-component.component.ts
+++ src/app/component/home-component/home-component.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthServiceService } from '../../services/auth-service.service';
 import { SocketService } from '../../services/socketio.service';
 import { Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environment';
 
 @Component({
   selector: 'app-home-component',
@@ -9,7 +11,7 @@ import { Subscription } from 'rxjs';
   styleUrl: './home-component.component.css',
 })
 export class HomeComponentComponent implements OnInit  {
-  constructor(private authservice: AuthServiceService, private _socketService:SocketService) {}
+  constructor(private authservice: AuthServiceService, private _socketService:SocketService, private _http:HttpClient) {}
   name: string = '';
   picture: string = '';
   notificationCount: number = 0;
@@ -18,6 +20,12 @@ export class HomeComponentComponent implements OnInit  {
   ngOnInit() {
     const loggedInUser = sessionStorage.getItem('loginedInUser');
     if (loggedInUser) this.name = JSON.parse(loggedInUser).username;
+
+    this._http.get(`${environment.apiUrl}/notification/getNotificationCount`).subscribe((data) => {
+      console.log(data);
+    },(err)=>{
+      console.log(err)
+    });
 
     console.log(loggedInUser);
     const ProfilePic = sessionStorage.getItem('loginedInUser');
