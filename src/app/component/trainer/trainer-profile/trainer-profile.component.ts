f6396d005783752903a5b73484a1055e26b00238
Sharath <sharathj116@gmail.com>
2024-07-06 11:05:08 +0530

    slot consoled

--- src/app/component/trainer/trainer-profile/trainer-profile.component.ts
+++ src/app/component/trainer/trainer-profile/trainer-profile.component.ts
@@ -102,10 +102,13 @@ export class TrainerProfileComponent implements OnInit {
      }
      this._http.get(`${environment.apiUrl}/trainer/CheckSlotBooked/${this.selectedSlot._id}`).subscribe((data: any) => {
        this.checkBoooking = data;
-       console.log("cccccccccccccccccccccccccccc",this.checkBoooking);
+       console.log("checkingslot",this.checkBoooking);
      }, (err)=>{
         console.log("ccceee",err);
      })
+     const slotUnAvailable = this.checkBoooking.find((slot) => slot._id === this.selectedSlot._id).status!=='booked';
+     console.log("slotUnAvailable",slotUnAvailable);
+     
      
 
     var handler = (<any>window).StripeCheckout.configure({
